7d22c9871b64058a7291745660850ecbfabf5624
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-07 22:29:31 +0200

    first commit

---
+++ videoflix/content/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import date
+
+class Video(models.Model):
+    created_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=500)
+    video_file = models.FileField(upload_to='videos', blank=True, null=True)
+
+def __str__(self):
+    return '[' + self.section.module.title + '] - ' + self.title
