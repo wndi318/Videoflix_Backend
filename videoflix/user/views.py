331dd545632f1c300670566d05760fbd0088bae8
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-11 10:50:50 +0200

    added verification email-template

--- videoflix/user/views.py
+++ videoflix/user/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.core.mail import send_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from .models import CustomUser
 from .serializers import RegisterSerializer
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 class RegisterView(APIView):
     def post(self, request):
@@ -14,23 +16,27 @@ class RegisterView(APIView):
         if serializer.is_valid():
             user = serializer.save()
 
-            # Verifizierungslink erstellen
             current_site = get_current_site(request)
             verification_url = reverse('verify_email', kwargs={'token': user.email_verification_token})
             full_verification_url = f"http://{current_site.domain}{verification_url}"
 
-            # E-Mail an den Benutzer senden
-            send_mail(
-                'Verify your email address',
-                f'Hello {user.email},\n\nPlease Verify your mail here:\n{full_verification_url}',
-                'kontakt@steffen-winter.org',  # Absender
-                [user.email],  # Empfänger
-                fail_silently=False,
+            html_content = render_to_string('email_verification_template.html', {'user': user, 'verification_url': full_verification_url})
+            text_content = strip_tags(html_content)
+
+
+            msg = EmailMultiAlternatives(
+                subject=' Verify your email address',
+                body = text_content,
+                from_email ='kontakt@steffen-winter.org',
+                to = [user.email]
             )
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
 
             return Response({'message': 'Check your Email for verification link'}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     
 class VerifyEmailView(APIView):
     def get(self, request, token):
@@ -39,9 +45,8 @@ class VerifyEmailView(APIView):
         except CustomUser.DoesNotExist:
             return Response({'error': 'Invalid verification token.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Benutzer verifizieren
         user.is_verified = True
-        user.email_verification_token = None  # Optional: Token nach Nutzung löschen
+        user.email_verification_token = None
         user.save()
 
         return Response({'message': 'Email successfully verified. You can now log in.'}, status=status.HTTP_200_OK)
