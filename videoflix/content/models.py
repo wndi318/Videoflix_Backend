90f76d560b56487e74173f294b086e9fda1d8db4
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-17 18:07:30 +0200

    added categorys on video model and also added new on videoflix boolean

--- videoflix/content/models.py
+++ videoflix/content/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from datetime import date
 
 class Video(models.Model):
+    GROUP_CHOICES = (
+        ('documentary', 'Documentary'),
+        ('drama', 'Drama'),
+        ('romance', 'Romance'),
+    )
+    
     created_at = models.DateField(default=date.today)
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=500)
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
+    group = models.CharField(max_length=20, choices=GROUP_CHOICES, default="documentary")
+    new_on_videoflix = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
