67dc6c8b5ea78d196f5bb61b18f84e91c96244da
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-21 00:09:45 +0200

    some changes for the quality select in frontend

--- videoflix/content/serializers.py
+++ videoflix/content/serializers.py
@@ -3,29 +3,47 @@ from django.conf import settings
 from .models import Video, Video480p, Video720p, Video1080p
 
 class VideoSerializer(serializers.ModelSerializer):
-    video_480p = serializers.StringRelatedField()
-    video_720p = serializers.StringRelatedField()
-    video_1080p = serializers.StringRelatedField()
+    video_480p_url = serializers.SerializerMethodField()
+    video_720p_url = serializers.SerializerMethodField()
+    video_1080p_url = serializers.SerializerMethodField()
     thumbnail_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Video
         fields = [
-            'id', 
-            'title', 
-            'description', 
-            'video_file', 
-            'thumbnail_url', 
-            'group', 
-            'new_on_videoflix', 
-            'created_at', 
-            'video_480p', 
-            'video_720p', 
-            'video_1080p'
+            'id',
+            'title',
+            'description',
+            'video_file',
+            'thumbnail_url',
+            'group',
+            'new_on_videoflix',
+            'created_at',
+            'video_480p_url',
+            'video_720p_url',
+            'video_1080p_url'
         ]
 
     def get_thumbnail_url(self, obj):
         request = self.context.get('request')
         if request and obj.thumbnail:
             return request.build_absolute_uri(obj.thumbnail.url)
+        return None
+
+    def get_video_480p_url(self, obj):
+        request = self.context.get('request')
+        if hasattr(obj, 'video_480p') and obj.video_480p and obj.video_480p.video_file_480p:
+            return request.build_absolute_uri(obj.video_480p.video_file_480p.url)
+        return None
+
+    def get_video_720p_url(self, obj):
+        request = self.context.get('request')
+        if hasattr(obj, 'video_720p') and obj.video_720p and obj.video_720p.video_file_720p:
+            return request.build_absolute_uri(obj.video_720p.video_file_720p.url)
+        return None
+
+    def get_video_1080p_url(self, obj):
+        request = self.context.get('request')
+        if hasattr(obj, 'video_1080p') and obj.video_1080p and obj.video_1080p.video_file_1080p:
+            return request.build_absolute_uri(obj.video_1080p.video_file_1080p.url)
         return None
