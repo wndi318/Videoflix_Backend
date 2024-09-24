368373499ef29bb51a010dd2796a7a44ad8d918a
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-24 14:43:56 +0200

    deleted media url

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -1,23 +1,5 @@
-"""
-URL configuration for videoflix project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
 from user.views import LoginView, LogoutView, RegisterView, VerifyEmailView, PasswordResetRequestView, PasswordResetConfirmView
 from content.views import VideoListView, VideoDetailView
@@ -33,4 +15,4 @@ urlpatterns = [
     path('reset-password-confirm/<int:user_id>/<str:token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('videos/', VideoListView.as_view(), name='video-list'),
     path('videos/<int:pk>/', VideoDetailView.as_view(), name='video-detail'),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + debug_toolbar_urls()
+] + debug_toolbar_urls()
